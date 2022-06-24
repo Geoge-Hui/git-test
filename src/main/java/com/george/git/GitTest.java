d9824e5784dd7542c7ff68c5b79d0ee31e9e8fea
George <m15102344783@163.com>
2022-06-24 16:27:02 +0800

    second commit

--- src/main/java/com/george/git/GitTest.java
+++ src/main/java/com/george/git/GitTest.java
@@ -2,6 +2,7 @@ package com.george.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git !!!");
+        System.out.println("hello git !!!1");
+        System.out.println("hello git !!!2");
     }
 }
