599634f416c2c5c23c3f183990462611559300bf
George <m15102344783@163.com>
2022-06-24 21:04:18 +0800

    push forth commit

--- src/main/java/com/george/git/GitTest.java
+++ src/main/java/com/george/git/GitTest.java
@@ -8,6 +8,9 @@ public class GitTest {
         System.out.println("hello git !!!4");
         System.out.println("master test !!");
         System.out.println("hot-fix test !!");
-        System.out.println("push test !!");
+        System.out.println("push test1 !!");
+        System.out.println("push test2 !!");
+        System.out.println("push test3 !!");
+        System.out.println("push test4 !!");
     }
 }
