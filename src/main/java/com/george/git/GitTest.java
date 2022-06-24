3a2eb571817c275810d6e042efefddfebb051ac1
George <m15102344783@163.com>
2022-06-24 21:16:56 +0800

    gitee first commit

--- src/main/java/com/george/git/GitTest.java
+++ src/main/java/com/george/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("push test3 !!");
         System.out.println("push test4 !!");
         System.out.println("pull test !!");
+        System.out.println("gitee test !!");
+
     }
 }
